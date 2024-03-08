2e2784fadcfc6fa0fa3fece1156c98f610ae25b5
Martin Knop <martinkf@live.com>
2024-03-08 00:34:53 -0300

    Sanitizing: SongPreview

--- BGAnimations/ScreenSelectMusicFull underlay/SongPreview.lua
+++ BGAnimations/ScreenSelectMusicFull underlay/SongPreview.lua
@@ -272,4 +272,175 @@ t[#t+1] = Def.ActorFrame {
     }
 }
 
+----
+-- vvvv POI PROJECT vvvv
+----
+
+local usingPOIUX = LoadModule("Config.Load.lua")("ActivatePOIProjectUX", "Save/OutFoxPrefs.ini") or false
+if usingPOIUX then
+	-- levers
+	local FrameW2 = 1600
+	local FrameH2 = 900	
+	
+	t = Def.ActorFrame {
+		OnCommand=function(self)
+			self:zoom(0.8)		
+		end,
+		
+		-- noise fx when switching song
+		Def.ActorFrame {
+			Name="Noise",
+
+			Def.Sprite {
+				Texture=THEME:GetPathG("", "Noise"),
+				InitCommand=function(self)
+					self:zoomto(FrameW2, FrameH2):y(125)
+					:texcoordvelocity(24,16)
+				end
+			},
+
+			Def.BitmapText {
+				Name="NextPrevText",
+				Font="VCR OSD Mono 40px",
+				Text="AAAAAAAAAAAAA",
+				InitCommand=function(self)
+					self:zoom(2):xy(0,-200)
+					:shadowlength(4)					
+				end,
+				ScrollMessageCommand=function(self, params)
+					local direction = params.Direction
+					if direction == -1 then
+						self:stoptweening()
+						:settext("PREV")
+						:zoom(2.1)
+						:easeoutquad(0.2)
+						:zoom(2)
+					elseif direction == 1 then
+						self:stoptweening()
+						:settext("NEXT")
+						:zoom(2.1)
+						:easeoutquad(0.2)
+						:zoom(2)
+					end					
+				end,
+			},
+		},
+
+		-- fullscreen bga_P
+		Def.Sprite {
+			InitCommand=function(self) self:Load(nil):queuecommand("Refresh") end,
+			CurrentSongChangedMessageCommand=function(self) self:Load(nil):queuecommand("Refresh") end,
+
+			RefreshCommand=function(self)
+				self:stoptweening():diffusealpha(0):sleep(PreviewDelay)
+				Song = GAMESTATE:GetCurrentSong()
+				if Song then
+					if GAMESTATE:GetCurrentSong():GetPreviewVidPath() == nil or LoadModule("Config.Load.lua")("ImagePreviewOnly", "Save/OutFoxPrefs.ini") then
+						self:queuecommand("LoadBG")
+					else
+						self:queuecommand("LoadAnimated")
+					end
+				end
+			end,
+
+			LoadBGCommand=function(self)
+				local Path = Song:GetBackgroundPath()
+				if Path and FILEMAN:DoesFileExist(Path) then
+					self:LoadFromCached("Background", Path):zoomto(FrameW2, FrameH2):y(125)
+					:linear(PreviewDelay):diffusealpha(1):diffuse(color("#ffffff"))
+				else
+					self:LoadFromCached("Banner", Song:GetBannerPath()):zoomto(FrameW2, FrameH2):y(125)
+					:linear(PreviewDelay):diffusealpha(1):diffuse(color("#ffffff"))
+				end
+			end,
+
+			LoadAnimatedCommand=function(self)
+				local Path = Song:GetPreviewVidPath()
+				if Path and FILEMAN:DoesFileExist(Path) then
+					self:Load(Path):zoomto(FrameW2, FrameH2):y(125)
+					:linear(PreviewDelay):diffusealpha(1):diffuse(color("#ffffff"))
+				else
+					self:queuecommand("LoadBG")
+				end
+			end,
+			
+			SongChosenMessageCommand=function(self)				
+				self:stoptweening():easeoutexpo(1):diffuse(color("#333333"))
+			end,
+			
+			SongUnchosenMessageCommand=function(self)				
+				self:stoptweening():easeoutexpo(0.5):diffuse(color("#ffffff"))
+			end
+		},
+		
+		-- smaller scale bga_P
+		Def.Sprite {
+			InitCommand=function(self) self:Load(nil):queuecommand("Refresh") end,
+			CurrentSongChangedMessageCommand=function(self) self:Load(nil):queuecommand("Refresh") end,
+
+			RefreshCommand=function(self)
+				self:stoptweening():diffusealpha(0):sleep(PreviewDelay)
+				Song = GAMESTATE:GetCurrentSong()
+				if Song then
+					if GAMESTATE:GetCurrentSong():GetPreviewVidPath() == nil or LoadModule("Config.Load.lua")("ImagePreviewOnly", "Save/OutFoxPrefs.ini") then
+						self:queuecommand("LoadBG")
+					else
+						self:queuecommand("LoadAnimated")
+					end
+				end
+			end,
+
+			LoadBGCommand=function(self)
+				local Path = Song:GetBackgroundPath()
+				if Path and FILEMAN:DoesFileExist(Path) then
+					self:LoadFromCached("Background", Path):zoomto(FrameW2/8, FrameH2/6.1):y(332):diffusealpha(0)
+					:linear(PreviewDelay):diffusealpha(1)
+				else
+					self:LoadFromCached("Banner", Song:GetBannerPath()):zoomto(FrameW2/8, FrameH2/6.1):y(332):diffusealpha(0)
+					:linear(PreviewDelay):diffusealpha(1)
+				end
+			end,
+
+			LoadAnimatedCommand=function(self)
+				local Path = Song:GetPreviewVidPath()
+				if Path and FILEMAN:DoesFileExist(Path) then
+					self:Load(Path):zoomto(FrameW2/8, FrameH2/6.1):y(332):diffusealpha(0)
+					:linear(PreviewDelay):diffusealpha(1)
+				else
+					self:queuecommand("LoadBG")
+				end
+			end,
+			
+			SongChosenMessageCommand=function(self)				
+				self:stoptweening():easeoutexpo(1):zoomto(FrameW2/3, FrameH2/3):y(-18):diffusealpha(1)
+			end,
+			SongUnchosenMessageCommand=function(self)				
+				self:stoptweening():easeoutexpo(0.5):zoomto(FrameW2/8, FrameH2/6.1):y(332):diffusealpha(0)
+			end
+		}
+	}
+	
+	-- song stats
+	t[#t+1] = Def.ActorFrame {
+		InitCommand=function(self) end,
+		CurrentSongChangedMessageCommand=function(self) self:playcommand("Refresh") end,
+
+		LoadActor("../ScreenEvaluation underlay/EvalSongInfo") .. {
+			InitCommand=function(self)
+				self:xy(0, 186)
+			end,
+			CurrentSongChangedMessageCommand=function(self)
+				self:stoptweening():diffusealpha(0):sleep(0.25):easeoutexpo(1):diffusealpha(1)
+			end,
+			SongChosenMessageCommand=function(self)            
+				self:stoptweening():diffusealpha(1):easeoutexpo(1):y(-220)
+			end,
+			SongUnchosenMessageCommand=function(self)            
+				self:stoptweening():diffusealpha(1):easeoutexpo(0.5):y(186)
+			end
+		}
+	}
+	
+end
+
 return t
