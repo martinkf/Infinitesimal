98cc8896a46b7fe342a02020efc33e1e76f335f6
Martin Knop <martinkf@live.com>
2024-03-07 23:40:24 -0300

    Sanitizing: MusicWheel

--- BGAnimations/ScreenSelectMusicFull underlay/MusicWheel.lua
+++ BGAnimations/ScreenSelectMusicFull underlay/MusicWheel.lua
@@ -284,4 +284,250 @@ for i = 1, WheelSize do
     }
 end
 
+----
+-- vvvv POI PROJECT vvvv
+----
+
+local usingPOIUX = LoadModule("Config.Load.lua")("ActivatePOIProjectUX", "Save/OutFoxPrefs.ini") or false
+if usingPOIUX then
+	-- levers
+	WheelSize = 26
+	WheelItem = { Width = 160, Height = 120 }
+	WheelSpacing = 180
+	WheelRotation = -0.025
+	local curvature = -90
+	local fieldOfView = 90
+	local yValue = 196
+	
+	t = Def.ActorFrame {
+		InitCommand=function(self)        
+			self:y(SCREEN_HEIGHT / 2 + yValue):fov(fieldOfView):SetDrawByZPosition(true)
+		:vanishpoint(SCREEN_CENTER_X, SCREEN_BOTTOM - 150 + curvature)
+			UpdateItemTargets(SongIndex)
+		end,
+
+		OnCommand=function(self)
+			GAMESTATE:SetCurrentSong(Songs[SongIndex])
+			SCREENMAN:GetTopScreen():AddInputCallback(InputHandler)
+			
+			self:easeoutexpo(1):y(SCREEN_HEIGHT / 2 - yValue)
+		end,
+		
+		-- Race condition workaround (yuck)
+		MusicWheelStartMessageCommand=function(self) self:sleep(0.01):queuecommand("Confirm") end,
+		ConfirmCommand=function(self) MESSAGEMAN:Broadcast("SongChosen") end,
+
+		-- These are to control the functionality of the music wheel
+		SongChosenMessageCommand=function(self)
+			--self:stoptweening():easeoutexpo(1):y(SCREEN_HEIGHT / 2 + yValue)
+			self:stoptweening():easeoutexpo(1):vanishpoint(SCREEN_CENTER_X, SCREEN_BOTTOM - 150 + 7000):y(-476):zoom(2):x(-640)
+			:playcommand("Busy")
+		end,
+		SongUnchosenMessageCommand=function(self)
+			--self:stoptweening():easeoutexpo(0.5):y(SCREEN_HEIGHT / 2 - yValue)
+			self:stoptweening():easeoutexpo(0.5):vanishpoint(SCREEN_CENTER_X, SCREEN_BOTTOM - 150 + curvature):y(SCREEN_HEIGHT / 2 - yValue):zoom(1):x(0)
+			:playcommand("NotBusy")
+		end,
+		
+		OpenGroupWheelMessageCommand=function(self) IsBusy = true end,
+		CloseGroupWheelMessageCommand=function(self, params)
+			if params.Silent == false then
+				-- Grab the new list of songs from the selected group
+				Songs = GroupsList[GroupIndex].SubGroups[SubGroupIndex].Songs
+				-- Reset back to the first song of the list
+				SongIndex = 1
+				GAMESTATE:SetCurrentSong(Songs[SongIndex])
+			end
+			-- Update wheel yada yada
+			UpdateItemTargets(SongIndex)
+			MESSAGEMAN:Broadcast("ForceUpdate")
+			self:sleep(0.01):queuecommand("NotBusy")
+		end,
+		
+		BusyCommand=function(self) IsBusy = true end,
+		NotBusyCommand=function(self) IsBusy = false end,
+		
+		-- Play song preview (thanks Luizsan)
+		Def.Actor {
+			CurrentSongChangedMessageCommand=function(self)
+				SOUND:StopMusic()
+				self:stoptweening():sleep(0.25):queuecommand("PlayMusic")
+			end,
+			
+			PlayMusicCommand=function(self)
+				local Song = GAMESTATE:GetCurrentSong()
+				if Song then
+					if ChartPreview then
+						local StepList = Song:GetAllSteps()
+						local FirstStep = StepList[1]
+						local Duration = FirstStep:GetChartLength()
+						SOUND:PlayMusicPart(Song:GetMusicPath(), Song:GetSampleStart(), 
+						(Duration - Song:GetSampleStart()), 0, 1, false, false, false, Song:GetTimingData())
+					else
+						SOUND:PlayMusicPart(Song:GetMusicPath(), Song:GetSampleStart(), 
+						Song:GetSampleLength(), 0, 1, false, false, false, Song:GetTimingData())
+					end
+				end
+			end
+		},
+
+		Def.Sound {
+			File=THEME:GetPathS("MusicWheel", "change"),
+			IsAction=true,
+			ScrollMessageCommand=function(self) self:play() end
+		},
+
+		Def.Sound {
+			File=THEME:GetPathS("Common", "Start"),
+			IsAction=true,
+			MusicWheelStartMessageCommand=function(self) self:play() end
+		},
+	}
+
+	-- The Wheel: originally made by Luizsan
+	for i = 1, WheelSize do
+
+		t[#t+1] = Def.ActorFrame{
+			OnCommand=function(self)
+				-- Load banner
+				UpdateBanner(self:GetChild("Banner"), Songs[Targets[i]])
+
+				-- Set initial position, Direction = 0 means it won't tween
+				self:playcommand("Scroll", {Direction = 0})
+			end,
+			
+			ForceUpdateMessageCommand=function(self)
+				-- Load banner
+				UpdateBanner(self:GetChild("Banner"), Songs[Targets[i]])
+				
+				--SCREENMAN:SystemMessage(GroupsList[GroupIndex].Name)
+
+				-- Set initial position, Direction = 0 means it won't tween
+				self:playcommand("Scroll", {Direction = 0})
+			end,
+
+			ScrollMessageCommand=function(self,param)
+				self:stoptweening()
+
+				-- Calculate position
+				local xpos = SCREEN_CENTER_X + (i - WheelCenter) * WheelSpacing
+
+				-- Calculate displacement based on input
+				local displace = -param.Direction * WheelSpacing
+
+				-- Only tween if a direction was specified
+				local tween = param and param.Direction and math.abs(param.Direction) > 0
+				
+				-- Adjust and wrap actor index
+				i = i - param.Direction
+				while i > WheelSize do i = i - WheelSize end
+				while i < 1 do i = i + WheelSize end
+
+				-- If it's an edge item, load a new banner. Edge items should never tween
+				if i == 1 or i == WheelSize then
+					UpdateBanner(self:GetChild("Banner"), Songs[Targets[i]])
+				elseif tween then
+					self:easeoutexpo(0.4)
+				end
+
+				-- Animate!
+				self:xy(xpos + displace, SCREEN_CENTER_Y)
+				self:rotationy((SCREEN_CENTER_X - xpos - displace) * -WheelRotation)
+				self:z(-math.abs(SCREEN_CENTER_X - xpos - displace) * 0.25)
+				self:GetChild(""):GetChild("Index"):playcommand("Refresh")
+				self:GetChild(""):GetChild("TestLabel"):playcommand("Refresh")
+			end,
+
+			Def.Banner {
+				Name="Banner",
+				SongChosenMessageCommand=function(self)				
+					self:stoptweening():easeoutexpo(1):diffusealpha(0)
+				end,
+				SongUnchosenMessageCommand=function(self)				
+					self:stoptweening():easeoutexpo(0.5):diffusealpha(1)
+				end
+			},
+
+			Def.Sprite {
+				Texture=THEME:GetPathG("", "MusicWheel/Res43SongFrame"),
+				SongChosenMessageCommand=function(self)				
+					self:stoptweening():easeoutexpo(1):zoomx(1.35)
+				end,
+				SongUnchosenMessageCommand=function(self)				
+					self:stoptweening():easeoutexpo(0.5):zoomx(1)
+				end
+			},
+
+			Def.ActorFrame {
+				SongChosenMessageCommand=function(self)
+					self:visible(false)
+				end,
+				SongUnchosenMessageCommand=function(self)
+					self:visible(true)
+				end,
+				
+				-- quad WheelSongNumber
+				Def.Quad {
+					InitCommand=function(self)
+						self:zoomto(60, 18):addy(73)
+						:diffuse(0,0,0,0.6)
+						:fadeleft(0.3):faderight(0.3)
+					end
+				},
+				-- text WheelSongNumber
+				Def.BitmapText {
+					Name="Index",
+					Font="Montserrat semibold 40px",
+					InitCommand=function(self)
+						self:addy(73):zoom(0.4):skewx(-0.1):diffusetopedge(0.95,0.95,0.95,0.8):shadowlength(1.5)
+					end,
+					RefreshCommand=function(self,param) self:settext(Targets[i]) end
+				},
+				
+				-- quad SongOrigin
+				Def.Quad {
+					InitCommand=function(self)
+						self:zoomto(140, 18):addy(-73)
+						:diffuse(0,0,0,0.6)
+						:fadeleft(0.2):faderight(0.2)
+					end
+				},
+				-- text SongOrigin
+				Def.BitmapText {				
+					Name="TestLabel",
+					Font="Montserrat semibold 40px",				
+					InitCommand=function(self)
+						self:addy(-73):zoom(0.4):skewx(-0.1):diffusetopedge(0.95,0.95,0.95,0.8):shadowlength(1.5)
+					end,
+					
+					RefreshCommand=function(self,param)
+						local colour = "#000000"
+						local song_origin = Songs[Targets[i]]:GetOrigin()
+						
+						if (song_origin == "The 1st DF") or (song_origin == "Premiere") or (song_origin == "Zero") or (song_origin == "Fiesta 2") then 			
+							colour = "#ff00ff" --pink
+						elseif (song_origin == "The 2nd DF") or (song_origin == "Rebirth") or (song_origin == "NX") or (song_origin == "Prime") or (song_origin == "Prime JE") then 
+							colour = "#1348ff" --blue
+						elseif (song_origin == "O.B.G The 3rd") or (song_origin == "Premiere 3") or (song_origin == "NX 2") or (song_origin == "NX 2 China") or (song_origin == "Prime 2") then 
+							colour = "#36b000" --green
+						elseif (song_origin == "O.B.G Season Evo.") or (song_origin == "Prex 3") or (song_origin == "NX Absolute") or (song_origin == "XX") then 
+							colour = "#ffff00" --yellow
+						elseif (song_origin == "Perfect") or (song_origin == "Exceed") or (song_origin == "Exceed S.E.") or (song_origin == "Fiesta") or (song_origin == "M") then 
+							colour = "#ff9900" --orange
+						elseif (song_origin == "Extra") or (song_origin == "Exceed 2") or (song_origin == "Fiesta EX") or (song_origin == "Phoenix") then
+							colour = "#ff0000" --red
+						elseif (song_origin == "Pro") or (song_origin == "Pro Encore") or (song_origin == "Pro 2") or (song_origin == "Infinity") or (song_origin == "Pump Jump") then
+							colour = "#aaaaaa" --gray
+						else
+							colour = "#fffffff" --white
+						end
+						
+						self:diffuse(color(colour)):settext(song_origin)
+					end
+				}
+			}
+		}
+	end
+end
+
 return t
