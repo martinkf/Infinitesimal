7d5e2fba9e3d60a7fee3e403e3d6041db182ef72
Martin Knop <martinkf@live.com>
2024-08-30 16:33:11 -0300

    Preliminary changes to SongTransition

--- BGAnimations/SongTransition.lua
+++ BGAnimations/SongTransition.lua
@@ -35,7 +35,27 @@ local t = Def.ActorFrame {
                 if GAMESTATE:GetCurrentSong() then
                     local Path = GAMESTATE:GetCurrentSong():GetBackgroundPath()
                     if Path and FILEMAN:DoesFileExist(Path) then
-                        self:Load(Path):scale_or_crop_background()
+                        self:Load(Path):scale_or_crop_background():diffusealpha(0.25)
+                    else
+                        self:Load(THEME:GetPathG("Common", "fallback background")):scale_or_crop_background()
+                    end
+                end
+            else
+                self:Load(nil)
+            end
+        end
+    },
+	
+	Def.Sprite {
+        OnCommand=function(self) self:playcommand("Refresh") end,
+        StartTransitioningCommand=function(self) self:playcommand("Refresh") end,
+
+        RefreshCommand=function(self)
+            if SCREENMAN:GetTopScreen():GetNextScreenName() ~= "ScreenSelectProfile" then
+                if GAMESTATE:GetCurrentSong() then
+                    local Path = GAMESTATE:GetCurrentSong():GetBannerPath()
+                    if Path and FILEMAN:DoesFileExist(Path) then
+                        self:Load(Path):scale_or_crop_background():zoom(0.75):addy(-40)
                     else
                         self:Load(THEME:GetPathG("Common", "fallback background")):scale_or_crop_background()
                     end
@@ -74,7 +94,7 @@ for pn in ivalues(GAMESTATE:GetHumanPlayers()) do
                     local ChartAuthor = Chart:GetAuthorCredit()
                     if ChartAuthor == "" then ChartAuthor = "Unknown" end
 
-                    self:GetChild("Ball"):diffuse(ChartTypeToColor(Chart)):diffusealpha(0) --disabling
+                    self:GetChild("Ball"):diffuse(ChartTypeToColor(Chart)):diffusealpha(1)
                     self:GetChild("Meter"):settext(ChartMeter)
                     self:GetChild("Credit"):settext(ChartAuthor)
                     self:GetChild("Difficulty"):settext(BasicMode and BasicChartLabel(Chart) or FullModeChartLabel(Chart))
@@ -100,7 +120,7 @@ for pn in ivalues(GAMESTATE:GetHumanPlayers()) do
                 Name="Frame",
                 Texture=THEME:GetPathG("", "UI/StepArtist" .. (pn == PLAYER_2 and "R" or "L")),
 				InitCommand=function(self)
-                    self:diffusealpha(0) --disabling
+                    self:diffusealpha(1)
                 end
             },
 
@@ -109,7 +129,7 @@ for pn in ivalues(GAMESTATE:GetHumanPlayers()) do
                 Texture=THEME:GetPathG("", "DifficultyDisplay/LargeBall"),
                 InitCommand=function(self)
                     self:xy(79.25 * PlayerDirection, 0.25):zoom(0.75)
-					:diffusealpha(0) --disabling
+					:diffusealpha(1)
                 end
             },
 
@@ -118,7 +138,7 @@ for pn in ivalues(GAMESTATE:GetHumanPlayers()) do
                 Texture=THEME:GetPathG("", "DifficultyDisplay/LargeTrim"),
                 InitCommand=function(self)
                     self:xy(79.25 * PlayerDirection, 0.25):zoom(0.75)
-					:diffusealpha(0) --disabling
+					:diffusealpha(1)
                 end
             },
 
@@ -127,7 +147,7 @@ for pn in ivalues(GAMESTATE:GetHumanPlayers()) do
                 Font="Montserrat numbers 40px",
                 InitCommand=function(self)
                     self:xy(79.25 * PlayerDirection, 1.25):zoom(0.88)
-					:diffusealpha(0) --disabling
+					:diffusealpha(1)
                 end
             },
 
@@ -136,7 +156,7 @@ for pn in ivalues(GAMESTATE:GetHumanPlayers()) do
                 Texture=THEME:GetPathG("", "DifficultyDisplay/Labels"),
                 InitCommand=function(self)
                     self:xy(79.25 * PlayerDirection, 23.25):visible(false):animate(false)
-					:diffusealpha(0) --disabling
+					:diffusealpha(1)
                 end
             },
             
@@ -145,7 +165,7 @@ for pn in ivalues(GAMESTATE:GetHumanPlayers()) do
                 Name="Difficulty",
                 InitCommand=function(self)
                     self:xy(79.25 * PlayerDirection, -21.25):visible(true):zoom(0.7):maxwidth(76):shadowlength(2):skewx(-0.1)
-					:diffusealpha(0) --disabling
+					:diffusealpha(1)
                 end
             },
 
@@ -158,7 +178,7 @@ for pn in ivalues(GAMESTATE:GetHumanPlayers()) do
                     :wrapwidthpixels(150)
                     :maxheight(40)
                     :maxwidth(150)
-					:diffusealpha(0) --disabling
+					:diffusealpha(1)
                 end
             }
         }
